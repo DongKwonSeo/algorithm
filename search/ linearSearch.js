1ce64b477c15ac4c1b194974ddec5901057e111f
DongKwon <gun100491@gmail.com>
2022-11-30 17:29:19 +0900

    LinearSearch add

---
+++ search/ linearSearch.js
@@ -0,0 +1,12 @@
+
+// 선형 검색
+function linearSearch(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+linearSearch([32, 56, 1, 2], 1);
