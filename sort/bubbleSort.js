318679c4cecee3876cc8d072aad3fb411e09769f
DongKwon <gun100491@gmail.com>
2022-12-01 23:35:36 +0900

    bubbleSort refactoring

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,14 +1,37 @@
+// function bubbleSort(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       console.log(arr, arr[i], arr[j + 1]);
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//     console.log("ONE PASS COMPLETE!");
+//   }
+//   return arr;
+// }
+
+// 리팩토링
 function bubbleSort(arr) {
   for (let i = arr.length; i > 0; i--) {
+    let noSwaps;
     for (let j = 0; j < i - 1; j++) {
+      noSwaps = true;
       console.log(arr, arr[i], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
-    console.log("ONE PASS COMPLETE!");
+    // console.log("ONE PASS COMPLETE!")
+    if (noSwaps) break;
   }
   return arr;
 }
+
+bubbleSort([33, 45, 11, 21, 55, 21]);
+refactoring
