b0acbb1d2774f5d2182b4ad67a4e2c86a7347551
DongKwon <gun100491@gmail.com>
2022-12-07 21:37:26 +0900

    doublyLinkedList get,set add

--- list/doublyLinkedList.js
+++ list/doublyLinkedList.js
@@ -70,4 +70,37 @@ class DoublyLinkedList {
       newNode.next = this.head;
     }
   }
+  // Get을 사용하는 이유는 배열과 다르게 index 번호를 사용 불가능해서
+  // List 돌면서 하나하나 가지고 오면 성능에 좋지 못하기 때문에 Get을 사용
+  get(index) {
+    // 정해진 범위 밖으로 나갈 수는 없다
+    if (idex < 0 || index >= this.length) return null;
+    let count, current;
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count != index) {
+        current = current.next;
+        count++;
+      }
+      return current;
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+      return current;
+    }
+  }
+  // Set 해당 노드의 값을 우리가 입력한 값으로 바꿔준 다음에 참을 출력한다.
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode != null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
